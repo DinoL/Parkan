f39445d309ca03c133fa6d86e24718c8523f91c7
DinoL <grech.leonid@gmail.com>
2017-09-08 23:54:48 +0300

    [+] Some NGB texture methods

--- texture_ngb.py
+++ texture_ngb.py
@@ -3,16 +3,16 @@ from texture import Texture
 class TextureNgb(Texture):
     def __init__(self, path):
         super().__init__(path)
-        print("Created NGB")
+        self.header_size = 16
 
     def save(self, id, out_file, palette):
         """To be implemented in derived classes"""
         print("NGB save method call")
 
     def get_header_and_texture(self):
-        """To be implemented in derived classes"""
-        print("NGB get_header_and_texture method call")
+        return self.seq[:self.header_size], self.seq[self.header_size:]
 
     def get_width_and_height(self):
-        """To be implemented in derived classes"""
-        print("NGB get_width_and_height method call")
+        wd = self.seq[4] - self.seq[0] + 1
+        ht = self.seq[6] - self.seq[2] + 1
+        return wd, ht
