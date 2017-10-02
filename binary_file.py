bdc31d5762cba16e8345a324db94374c6a5855ce
DinoL <grech.leonid@gmail.com>
2017-10-02 23:30:00 +0300

    [+] Add methods for binary file

--- binary_file.py
+++ binary_file.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+
 class BinaryFile:
     def __init__(self, path):
         f = open(path, "r")
         self.seq = np.fromfile(f, dtype=np.uint8)
+        self.word_size = 2
 
     @staticmethod
     def bytes_seq_to_int(bytes_seq):
@@ -11,3 +13,10 @@ class BinaryFile:
         for i in range(len(bytes_seq)):
             sum += bytes_seq[i] * (256 ** i)
         return sum
+
+    def get_int(self, start, end):
+        return BinaryFile.bytes_seq_to_int(self.seq[start:end])
+
+    def get_word(self, start):
+        return self.get_int(start, start + self.word_size)
+
