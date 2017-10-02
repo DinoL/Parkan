7a521ee53bfa1143051cc4248c402a33066b80c0
DinoL <grech.leonid@gmail.com>
2017-10-02 23:32:50 +0300

    [*] Speed up texture calculation for NGB files

--- texture_ngb.py
+++ texture_ngb.py
@@ -1,5 +1,4 @@
 from texture import Texture
-from binary_file import BinaryFile
 import numpy as np
 import cv2
 
@@ -17,9 +16,9 @@ class TextureNgb(Texture):
 
     def get_width_and_height(self):
         header, _ = self.get_header_and_texture()
-        header_words = {i : BinaryFile.bytes_seq_to_int(header[i:i + 2]) for i in range(0, 8, 2)}
-        wd = header_words[4] - header_words[0] + 1
-        ht = header_words[6] - header_words[2] + 1
+        header_words = [self.get_word(self.word_size * i) for i in range(4)]
+        wd = header_words[2] - header_words[0] + 1
+        ht = header_words[3] - header_words[1] + 1
         return wd, ht
 
 
@@ -37,38 +36,78 @@ class TextureNgbPlain(TextureNgb):
 
 
 class TextureNgbComplex(TextureNgb):
+    def __init__(self, path):
+        super().__init__(path)
+        self.row_info_size = 4
+        self.row_info_pos_size = 4
+        self.terminal_value = 255
+        self.wd, self.ht = super().get_width_and_height()
+
     @staticmethod
     def has_signature(binary):
         return np.array_equal(binary.seq[12:16], [0xAB, 0xCD, 0xEF, 0x01])
 
-    def save(self, order, out_file, palette):
+    def get_width_and_height(self):
+        return self.wd, self.ht
+
+    def get_row_range(self, row):
+        row_info_position = self.get_row_info_position(row)
+        if self.is_terminal(row_info_position):
+            return row_info_position, row_info_position
+
+        _, ht = self.get_width_and_height()
+        if row == ht - 1:
+            return row_info_position, len(self.seq) - 4
+
+        next_row_info_position = self.get_row_info_position(row + 1)
+        return row_info_position, next_row_info_position - 2
+
+    def get_plain_index(self, x, y):
+        wd, _ = self.get_width_and_height()
+        return wd * y + x
+
+    @staticmethod
+    def insert(texture, pos, patch):
+        width = len(patch)
+        texture[pos : pos + width] = patch
+
+    def process_row(self, row, texture):
+        row_start, row_end = self.get_row_range(row)
+        if row_start >= row_end:
+            return texture
+
+        cur_x = 0
+        cur = row_start
+        while cur < row_end:
+            cur_wd, shift = self.get_row_info(cur)
+            cur_x += shift
+            cur += self.row_info_size
+            TextureNgbComplex.insert(texture, self.get_plain_index(cur_x, row), self.seq[cur : cur + cur_wd])
+            cur_x += cur_wd
+            cur += cur_wd
+        return texture
+
+    def process_rows(self):
         wd, ht = self.get_width_and_height()
         texture = [self.get_default_color()] * (wd * ht)
-        for y in range(ht):
-            for x in range(wd):
-                texture[wd * y + x] = self.get_pixel_color(x, y)
+        for row in range(ht):
+            self.process_row(row, texture)
+        return texture
 
+    def save(self, order, out_file, palette):
+        wd, ht = self.get_width_and_height()
+        texture = self.process_rows()
         pixels = [palette.get_color_by_id(col_id) for col_id in texture]
         arr = np.array(pixels).reshape(ht, wd, palette.get_used_channels_cnt())
         cv2.imwrite(out_file, arr)
 
-    def get_pixel_color(self, x, y):
-        row_info_pos_pos = 4*y + self.header_size
-        row_info_pos = BinaryFile.bytes_seq_to_int(self.seq[row_info_pos_pos : row_info_pos_pos + 4])
-        row_info = BinaryFile.bytes_seq_to_int(self.seq[row_info_pos: row_info_pos + 4])
-        mod = 2**16
-        cur_wd, x_shift = divmod(row_info, mod)
-        while True:
-            cur_x = x - x_shift
-            if cur_x < 0:
-                break
+    def get_row_info_position(self, row):
+        s = self.header_size + self.row_info_pos_size * row
+        return self.get_int(s, s + self.row_info_pos_size)
 
-            if cur_x < cur_wd:
-                return self.seq[row_info_pos + cur_x + 4]
-            else:
-                row_info_pos += (cur_wd + 4)
-                row_info = BinaryFile.bytes_seq_to_int(self.seq[row_info_pos : row_info_pos + 4])
-                x = cur_x - cur_wd
-                cur_wd, x_shift = divmod(row_info, mod)
+    def get_row_info(self, pos):
+        return divmod(self.get_int(pos, pos + self.row_info_size), 2 ** 16)
 
-        return self.get_default_color()
+    def is_terminal(self, pos):
+        return pos >= len(self.seq) or\
+               (self.seq[pos] == self.terminal_value and self.seq[pos + 1] == self.terminal_value)
