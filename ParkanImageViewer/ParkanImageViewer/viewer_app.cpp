a1badc907e3fb0d14c10531c21e0776d6370993b
DinoL <grech.leonid@gmail.com>
2017-10-14 22:47:42 +0300

    [+] Add button to open hardcoded texture

--- ParkanImageViewer/ParkanImageViewer/viewer_app.cpp
+++ ParkanImageViewer/ParkanImageViewer/viewer_app.cpp
@@ -1,9 +1,11 @@
 #include "viewer_app.h"
 #include "ui_viewer_app.h"
 
+#include "color_ramp_widget.h"
+
 ViewerApp::ViewerApp(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::ViewerApp)
+    ui(new Ui::ViewerApp), m_crw(nullptr)
 {
     ui->setupUi(this);
 }
@@ -11,4 +13,11 @@ ViewerApp::ViewerApp(QWidget *parent) :
 ViewerApp::~ViewerApp()
 {
     delete ui;
+}
+
+void ViewerApp::on_pushButton_clicked()
+{
+    const QFileInfo file("P.COL");
+    const Palette palette(file);
+    m_crw.reset(new ColorRampWidget(palette));
 }
--- ParkanImageViewer/ParkanImageViewer/viewer_app.h
+++ ParkanImageViewer/ParkanImageViewer/viewer_app.h
@@ -2,6 +2,8 @@
 #define VIEWER_APP_H
 
 #include <QMainWindow>
+#include "color_ramp_widget.h"
+#include <memory>
 
 namespace Ui {
 class ViewerApp;
@@ -15,8 +17,13 @@ public:
     explicit ViewerApp(QWidget *parent = 0);
     ~ViewerApp();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::ViewerApp *ui;
+    std::unique_ptr<ColorRampWidget> m_crw;
+
 };
 
 #endif // VIEWER_APP_H
