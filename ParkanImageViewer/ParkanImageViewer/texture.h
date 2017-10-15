0ccd7132c06f74194aa11ee14b4546af4cb18c47
DinoL <grech.leonid@gmail.com>
2017-10-16 00:42:35 +0300

    [+] Add texture interface declaration

--- ParkanImageViewer/ParkanImageViewer/texture.h
+++ ParkanImageViewer/ParkanImageViewer/texture.h
@@ -2,11 +2,30 @@
 #define TEXTURE_H
 
 #include "binary_file.h"
+#include <palette.h>
+#include <Eigen/Core>
+
+using namespace Eigen;
 
 class Texture : public BinaryFile
 {
 public:
+    using ColorMatrix = Matrix<QColor, Dynamic, Dynamic>;
+    using ColorIdMatrix = MatrixXi;
+
     Texture(const QFileInfo& i_path);
+
+    ColorMatrix get_pixels(const Palette& i_palette);
+
+    void save(const QString& i_out_file, const Palette& i_palette);
+
+    ColorIdMatrix get_texture() const;
+
+    void get_header_and_body() const;
+
+    void get_width_and_height() const;
+
+    static bool has_signature(const BinaryFile& i_binary);
 };
 
 #endif // TEXTURE_H
