df4f2884e3788038adada49ec0f5eb3132d682d8
Leonid Grechishnikov <lgrechishnikov@aligntech.com>
2017-12-29 14:18:50 +0300

    [-] Fix sequence to bytes conversion, extract method

--- binary_file.py
+++ binary_file.py
@@ -15,11 +15,14 @@ class BinaryFile:
             sum += bytes_seq[i] * (256 ** i)
         return sum
 
+    def get_bytes(self, start, end):
+        return bytes(list(self.seq[start:end]))
+
     def get_int(self, start, end):
         return BinaryFile.bytes_seq_to_int(self.seq[start:end])
 
     def get_float(self, start, end):
-        return struct.unpack('f', bytes(self.seq[start:end]))[0]
+        return struct.unpack('f', self.get_bytes(start, end))[0]
 
     def get_word(self, start):
         return self.get_int(start, start + self.word_size)
