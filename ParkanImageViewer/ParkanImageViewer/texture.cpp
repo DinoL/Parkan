943cc3bd2340894ab9bf213732d89034e92465c2
DinoL <grech.leonid@gmail.com>
2018-01-21 01:00:02 +0300

    [*] Read texture parameters from file

--- ParkanImageViewer/ParkanImageViewer/texture.cpp
+++ ParkanImageViewer/ParkanImageViewer/texture.cpp
@@ -1,7 +1,15 @@
 #include "texture.h"
+#include "binary_stream.h"
+#include <fstream>
 
 Texture::Texture(const QFileInfo& i_path)
-    : BinaryFile(i_path)
 {
+    std::ifstream file(i_path.filePath().toStdString(), std::ios::binary);
+    if(file.fail())
+        throw "Cannot open texture file file " + i_path.filePath();
 
+    InputBinaryStream bis(file);
+
+    QByteArray signature(4, '\0');
+    bis >> signature >> m_width >> m_height;
 }
--- ParkanImageViewer/ParkanImageViewer/texture.h
+++ ParkanImageViewer/ParkanImageViewer/texture.h
@@ -1,31 +1,16 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-#include "binary_file.h"
-#include <palette.h>
-#include <Eigen/Core>
+#include <QFileInfo>
 
-using namespace Eigen;
-
-class Texture : public BinaryFile
+class Texture
 {
 public:
-    using ColorMatrix = Matrix<QColor, Dynamic, Dynamic>;
-    using ColorIdMatrix = MatrixXi;
-
     Texture(const QFileInfo& i_path);
 
-    ColorMatrix get_pixels(const Palette& i_palette);
-
-    void save(const QString& i_out_file, const Palette& i_palette);
-
-    ColorIdMatrix get_texture() const;
-
-    void get_header_and_body() const;
-
-    void get_width_and_height() const;
-
-    static bool has_signature(const BinaryFile& i_binary);
+private:
+    quint32 m_width = 0;
+    quint32 m_height = 0;
 };
 
 #endif // TEXTURE_H
