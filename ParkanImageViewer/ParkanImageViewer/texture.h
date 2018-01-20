88b878f2061e4b4985dfbdc6cc8c46e72923b4f6
DinoL <grech.leonid@gmail.com>
2018-01-21 01:14:01 +0300

    [+] Add getters for texture properties

--- ParkanImageViewer/ParkanImageViewer/texture.h
+++ ParkanImageViewer/ParkanImageViewer/texture.h
@@ -8,6 +8,9 @@ class Texture
 public:
     Texture(const QFileInfo& i_path);
 
+    quint32 width() const { return m_width; }
+    quint32 height() const { return m_height; }
+
 private:
     quint32 m_width = 0;
     quint32 m_height = 0;
