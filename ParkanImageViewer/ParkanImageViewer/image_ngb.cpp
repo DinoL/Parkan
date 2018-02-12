77f99eeb55e92473c2de8ebddd112d905545d159
Leonid Grechishnikov <lgrechishnikov@aligntech.com>
2018-02-12 15:52:35 +0300

    [-] Fix NGB size parsing

--- ParkanImageViewer/ParkanImageViewer/image_ngb.cpp
+++ ParkanImageViewer/ParkanImageViewer/image_ngb.cpp
@@ -4,20 +4,21 @@
 #include <QFile>
 
 #include <fstream>
+#include <vector>
 
 ImageNgb::ImageNgb(const QFileInfo& i_path)
     : Image(i_path)
 {
-    std::ifstream file(i_path.filePath().toStdString(), std::ios::binary | std::ios::ate);
+    std::ifstream file(i_path.filePath().toStdString(), std::ios::binary);
     if(file.fail())
         throw "Cannot open texture file " + i_path.filePath();
 
     InputBinaryStream bis(file);
 
-    qint16 words[8];
-    for(int i = 0; i < 8; ++i)
+    std::vector<quint16> words(8);
+    for(auto& word : words)
     {
-        bis >> words[i];
+        bis >> word;
     }
 
     m_width = words[2] - words[0] + 1;
@@ -25,5 +26,5 @@ ImageNgb::ImageNgb(const QFileInfo& i_path)
 
     m_data.resize(m_width * m_height);
 
-    m_img =  QImage((uchar*)m_data.data(), m_width, m_height, QImage::QImage::Format_Indexed8);
+    m_img = QImage((uchar*)m_data.data(), m_width, m_height, QImage::QImage::Format_Indexed8);
 }
