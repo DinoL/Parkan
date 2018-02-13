7a936bd0c43cc6b0ee5ebac0a243cc0e506c153e
DinoL <grech.leonid@gmail.com>
2018-02-14 01:20:56 +0300

    [-] Fix incorrect bytes to image conversion

--- ParkanImageViewer/ParkanImageViewer/image.cpp
+++ ParkanImageViewer/ParkanImageViewer/image.cpp
@@ -28,7 +28,16 @@ void Image::save(const QString& i_path) const
 bool Image::prepare_image()
 {
     if(m_width > 0 && m_height > 0 && m_data.size() >= m_width * m_height)
-        m_img = QImage((uchar*)m_data.data(), m_width, m_height, QImage::QImage::Format_Indexed8);
-
+    {
+        m_img = QImage(m_width, m_height, QImage::QImage::Format_Indexed8);
+        for(qint32 y = 0; y < m_height; ++y)
+        {
+            auto row = m_img.scanLine(y);
+            for(qint32 x = 0; x < m_width; ++x)
+            {
+                row[x] = m_data[x + m_width * y];
+            }
+        }
+    }
     return !m_img.isNull();
 }
