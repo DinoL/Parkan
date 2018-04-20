f8193b6ba60ace0afb2e0ee599eb27bb403d142a
DinoL <grech.leonid@gmail.com>
2018-04-21 00:40:06 +0300

    [+] Add missing header

--- ParkanImageViewer/ParkanImageViewer/geometry_importer.cpp
+++ ParkanImageViewer/ParkanImageViewer/geometry_importer.cpp
@@ -1,5 +1,7 @@
 #include "geometry_importer.h"
 
+#include <fstream>
+
 bool GeometryImporter::import_geometry(const QString& i_from, SerializableGeometry& o_geometry) const
 {
     if(i_from.isEmpty())
