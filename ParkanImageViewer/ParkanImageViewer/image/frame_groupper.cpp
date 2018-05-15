b9dffc4b728833d6d721765b5d3f1a2aedb65836
DinoL <grech.leonid@gmail.com>
2018-05-15 23:53:38 +0300

    [*] Refactor frame groupper

--- ParkanImageViewer/ParkanImageViewer/image/frame_groupper.cpp
+++ ParkanImageViewer/ParkanImageViewer/image/frame_groupper.cpp
@@ -1,6 +1,83 @@
 #include "frame_groupper.h"
 
-FrameGroupper::FrameGroupper()
-{
+#include <algorithm>
 
+QFileInfoList FrameGroupper::to_info_list(const FrameGroupper::FrameGroup& i_frames) const
+{
+    QFileInfoList paths;
+    for(const auto& frame : i_frames)
+    {
+        paths.push_back(frame.path());
+    }
+    return paths;
+}
+
+std::vector<ImageFrame> FrameGroupper::convert_to_frames(const QFileInfoList& i_paths) const
+{
+    std::vector<ImageFrame> result;
+    std::transform(i_paths.begin(), i_paths.end(), std::back_inserter(result),
+                   [](const QFileInfo& i_path){ return ImageFrame(i_path); });
+    return result;
+}
+
+std::vector<FrameGroupper::FrameGroup> FrameGroupper::split_by_groups(const QFileInfoList& i_paths) const
+{
+    if(i_paths.empty())
+        return {};
+
+    std::vector<ImageFrame> frames = convert_to_frames(i_paths);
+
+    if(i_paths.size() == 1)
+    {
+        return { frames };
+    }
+
+    std::vector<FrameGroup> all_groups;
+
+    FrameGroup cur_group;
+    for(const auto& frame : frames)
+    {
+        if(!merge(cur_group, frame))
+        {
+            add_group(all_groups, cur_group);
+            cur_group = { frame };
+        }
+    }
+    add_group(all_groups, cur_group);
+
+    return all_groups;
+}
+
+bool FrameGroupper::add_group(std::vector<FrameGroupper::FrameGroup>& io_all_groups, FrameGroupper::FrameGroup& io_group) const
+{
+    if(io_group.empty())
+        return false;
+
+    if(io_group.size() > 1)
+    {
+        auto by_number = [](const ImageFrame& i_lhs, const ImageFrame& i_rhs)
+        {
+            return i_lhs.number() < i_rhs.number();
+        };
+
+        // sort group by frame number
+        std::sort(io_group.begin(), io_group.end(), by_number);
+    }
+
+    io_all_groups.push_back(io_group);
+    return true;
+}
+
+bool FrameGroupper::merge(FrameGroupper::FrameGroup& io_group, const ImageFrame& i_frame) const
+{
+    if(io_group.empty() || !frames_are_in_one_group(io_group.back(), i_frame))
+        return false;
+
+    io_group.push_back(i_frame);
+    return true;
+}
+
+bool FrameGroupper::frames_are_in_one_group(const ImageFrame& i_first, const ImageFrame& i_second) const
+{
+    return i_first.name() == i_second.name();
 }
--- ParkanImageViewer/ParkanImageViewer/image/frame_groupper.h
+++ ParkanImageViewer/ParkanImageViewer/image/frame_groupper.h
@@ -1,8 +1,9 @@
 #ifndef FRAME_GROUPPER_H
 #define FRAME_GROUPPER_H
 
+#include "image_frame.h"
+
 #include <QFileInfo>
-#include <QDir>
 #include <QString>
 
 #include <vector>
@@ -10,86 +11,20 @@
 class FrameGroupper
 {
 public:
-    FrameGroupper();
+    using FrameGroup = std::vector<ImageFrame>;
 
-    using FrameGroup = QFileInfoList;
+    QFileInfoList to_info_list(const FrameGroup& i_frames) const;
 
-    std::vector<FrameGroup> split_by_groups(const QFileInfoList& i_paths) const
-    {
-        std::vector<FrameGroup> all_groups;
+    std::vector<ImageFrame> convert_to_frames(const QFileInfoList& i_paths) const;
 
-        FrameGroup cur_group = {};
-
-        for(const auto& path : i_paths)
-        {
-            if(!merge(cur_group, path))
-            {
-                if(!cur_group.isEmpty())
-                    all_groups.push_back(cur_group);
-
-                cur_group = { path };
-            }
-        }
-        if(!cur_group.empty())
-            all_groups.push_back(cur_group);
-
-        return all_groups;
-
-    }
+    std::vector<FrameGroup> split_by_groups(const QFileInfoList& i_paths) const;
 
 private:
-    bool merge(FrameGroup& io_group, const QFileInfo& i_path) const
-    {
-        if(io_group.empty() || !paths_are_sequential(io_group.back(), i_path))
-            return false;
+    bool add_group(std::vector<FrameGroup>& io_all_groups, FrameGroup& io_group) const;
 
-        io_group.push_back(i_path);
-        return true;
-    }
+    bool merge(FrameGroup& io_group, const ImageFrame& i_frame) const;
 
-    bool paths_are_sequential(const QFileInfo& i_first, const QFileInfo& i_second) const
-    {
-        if(i_first.absoluteDir().absolutePath() != i_second.absoluteDir().absolutePath())
-            return false;
-
-        const auto first_ext = i_first.suffix();
-        const auto second_ext = i_second.suffix();
-
-        if(first_ext == second_ext)
-        {
-            return names_are_sequential(i_first.baseName(), i_second.baseName());
-        }
-        else
-        {
-            return names_are_sequential(first_ext, second_ext);
-        }
-    }
-
-    bool names_are_sequential(const QString& i_first, const QString& i_second) const
-    {
-        int i = 0;
-        while(i < i_first.size() && i < i_second.size())
-        {
-            if(i_first[i] != i_second[i])
-                break;
-
-            ++i;
-        }
-
-        if (i == i_first.size() || i == i_second.size())
-            return false;
-
-        const QString first_end = i_first.right(i_first.size() - i);
-        const QString second_end = i_second.right(i_second.size() - i);
-
-        bool first_is_numeric, second_is_numeric;
-        const int first_id = first_end.toInt(&first_is_numeric, 10);
-        const int second_id = second_end.toInt(&second_is_numeric, 10);
-        if(!first_is_numeric || !second_is_numeric)
-            return false;
-
-        return second_id == first_id + 1;
-    }
+    bool frames_are_in_one_group(const ImageFrame& i_first, const ImageFrame& i_second) const;
 
 };
 
